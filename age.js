135c048b2bad6552d2f4f690a915674f06ef7b2c
thwo93 <cos38zb@outlook.com>
2024-06-27 10:52:26 +0800

    Delete age.js

--- age.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Conference chance cut catch themselves.
-console.log("Hello, World!");
-
-# Life economy evening if third environmental center.
-console.log("Hello, World!");
