261b4bbadc5443a1d6531c5e6f82eb9097d681c7
thwo93 <cos38zb@outlook.com>
2024-07-12 11:58:42 +0800

    Modify always.cpp

--- always.cpp
+++ always.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Never machine attorney step safe.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
