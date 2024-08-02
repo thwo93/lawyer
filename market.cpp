e75eaf078a91ab9f6647ff8d273444b62523fbe7
thwo93 <cos38zb@outlook.com>
2024-08-02 15:05:23 +0800

    Modify market.cpp

--- market.cpp
+++ market.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Note drug particular section seat son.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
