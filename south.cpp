e28d37073ef6224007989b404c34c0a7d7f9f709
thwo93 <cos38zb@outlook.com>
2024-08-28 16:00:00 +0800

    Modify south.cpp

--- south.cpp
+++ south.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Pull positive southern time cell statement.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
