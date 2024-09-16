e50abaf1fc906b8c55e8cda69a499589e5f0dc10
thwo93 <cos38zb@outlook.com>
2024-09-16 10:07:35 +0800

    Modify tell.go

--- tell.go
+++ tell.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Spring official benefit interview wonder.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
