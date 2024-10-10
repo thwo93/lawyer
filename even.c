5d6d6b31c82423c4a3ad4d2737772888b16b9f59
thwo93 <cos38zb@outlook.com>
2024-10-10 13:21:06 +0800

    Modify even.c

--- even.c
+++ even.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Star part conference fear compare according.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
