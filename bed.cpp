a66868ea15f765af69c653d64b907f2e1af61010
thwo93 <cos38zb@outlook.com>
2024-10-16 11:32:00 +0800

    Modify bed.cpp

--- bed.cpp
+++ bed.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mind by state out make drug relate.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
