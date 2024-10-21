f85201dd2cb060ac7eeed265a553b8f850a8f240
thwo93 <cos38zb@outlook.com>
2024-10-21 11:43:38 +0800

    Modify prepare.cpp

--- prepare.cpp
+++ prepare.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Generation ahead miss offer popular especially those.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
