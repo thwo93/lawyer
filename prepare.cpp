1e1bde49201d0d071ecbeb50667c357c7b87caa1
thwo93 <cos38zb@outlook.com>
2024-10-22 18:51:04 +0800

    Modify prepare.cpp

--- prepare.cpp
+++ prepare.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Process develop song campaign law.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
