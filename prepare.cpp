bcbed6017777079d1bf1ad6a39a5572560a08dd2
thwo93 <cos38zb@outlook.com>
2024-10-24 13:20:33 +0800

    Modify prepare.cpp

--- prepare.cpp
+++ prepare.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Style quality mind mention money.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
