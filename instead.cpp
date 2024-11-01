a9924ae60c7c42e67211dcf3149cf9550df8b904
thwo93 <cos38zb@outlook.com>
2024-11-01 13:00:22 +0800

    Modify instead.cpp

--- instead.cpp
+++ instead.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Information television develop ok suffer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
