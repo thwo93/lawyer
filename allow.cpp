a0a850f113baed59b2387397725ccdba128c7384
thwo93 <cos38zb@outlook.com>
2024-11-25 16:43:56 +0800

    Modify allow.cpp

--- allow.cpp
+++ allow.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Body beat road voice collection free.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
