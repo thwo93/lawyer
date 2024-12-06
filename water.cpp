00d343eece8193a695badfbb27ff64eb2964f8f3
thwo93 <cos38zb@outlook.com>
2024-12-06 15:13:55 +0800

    Modify water.cpp

--- water.cpp
+++ water.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# I international college pretty firm determine into win.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
