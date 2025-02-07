5a1f3fab00be30f66eb5081fd5310ea33946ec61
thwo93 <cos38zb@outlook.com>
2025-02-07 13:05:28 +0800

    Modify item.cpp

--- item.cpp
+++ item.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Process follow fish by.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
