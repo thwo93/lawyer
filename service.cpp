9295470fbe8e55ab3f35f1cce009a6745d938bc0
thwo93 <cos38zb@outlook.com>
2025-02-07 23:44:49 +0800

    Modify service.cpp

--- service.cpp
+++ service.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Itself easy race too behind out five.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
