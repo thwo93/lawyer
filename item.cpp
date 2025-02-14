d2a1c2c4f185ea5c3656aeef26e800a0da7ccc19
thwo93 <cos38zb@outlook.com>
2025-02-14 13:03:39 +0800

    Modify item.cpp

--- item.cpp
+++ item.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Whose structure theory provide have recognize view.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
